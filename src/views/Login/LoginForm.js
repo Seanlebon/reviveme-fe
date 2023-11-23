571876168596f818936716f3aa242714f697eaa6
SeanLeBon <sean.chen0818@gmail.com>
2023-11-23 01:45:32 -0800

    figuring out FE folder structure

---
+++ src/views/Login/LoginForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  // Your component logic goes here
+
+  render() {
+    return (
+      <div className="login-form">
+        <h1>Hey This is the Login form</h1>
+        {/* Your component content goes here */}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
