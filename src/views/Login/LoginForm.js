5e49d6a7e21e9ecb0b75669b0ff97f1d8d258c49
SeanLeBon <sean.chen0818@gmail.com>
2023-11-23 01:57:45 -0800

    adding basic bootstrap login form

--- src/views/Login/LoginForm.js
+++ src/views/Login/LoginForm.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 
-class LoginForm extends React.Component {
-  // Your component logic goes here
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
 
-  render() {
-    return (
-      <div className="login-form">
-        <h1>Hey This is the Login form</h1>
-        {/* Your component content goes here */}
-      </div>
-    );
-  }
-}
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-export default MyComponent;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your login logic here, e.g., make an API call to validate the credentials
+    console.log('Username:', formData.username);
+    console.log('Password:', formData.password);
+    // Reset the form after processing the data
+    setFormData({ username: '', password: '' });
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2 className="mb-4">Login Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            className="form-control"
+            required
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="form-control"
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
