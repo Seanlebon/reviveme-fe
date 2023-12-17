8ed211f11ceb1654f9b531378b0650024bc1fb4f
Shashank Anand Kannan <shashank.anandkannan@stackadapt.com>
2023-12-17 14:43:27 -0500

    Add reply editor

--- src/views/ThreadPage/Comment/Comment.tsx
+++ src/views/ThreadPage/Comment/Comment.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Comment as CommentType } from '../../../types/CommonTypes';
 import axios from '../../../apis/reviveme';
 import './Comment.css';
 import { AxiosError } from 'axios';
+import { useParams } from 'react-router-dom';
 
 interface CommentProps {
   comment: CommentType;
@@ -10,7 +11,10 @@ interface CommentProps {
 }
 
 const Comment: React.FC<CommentProps> = ({ comment, refetchComments }) => {
-  const handleDelete = () => {
+  const { id } = useParams<{ id: string }>(); // Thread id
+  const [showReplyEditor, setShowReplyEditor] = useState(false);
+
+  const handleDeleteButtonPress = () => {
     axios
       .delete(`/api/v1/comments/${comment.id}`)
       .then(refetchComments)
@@ -19,20 +23,61 @@ const Comment: React.FC<CommentProps> = ({ comment, refetchComments }) => {
       });
   };
 
+  const handleReplyButtonPress = () => {
+    setShowReplyEditor(true);
+  };
+
+  const handleReplyCancel = () => {
+    setShowReplyEditor(false);
+  };
+
+  const handleReplySubmit = () => {
+    axios.post(`/api/v1/thread/${id}/comments`, {});
+  };
+
   return (
     <div className='comment-card'>
       <p className='author-username'>
         {comment.deleted ? '[deleted]' : comment.author_username}
       </p>
       <p>{comment.deleted ? '[deleted]' : comment.content}</p>
+      {showReplyEditor && (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <textarea placeholder='Reply to this comment' />
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+            }}
+          >
+            <button
+              className='btn btn-sm btn-secondary'
+              onClick={handleReplyCancel}
+            >
+              Cancel
+            </button>
+            <button
+              className='btn btn-sm btn-primary'
+              onClick={handleReplySubmit}
+            >
+              Reply
+            </button>
+          </div>
+        </div>
+      )}
       <div className='comment-footer'>
         <button
           className='btn comment-footer-button-text'
-          onClick={handleDelete}
+          onClick={handleDeleteButtonPress}
         >
           delete
         </button>
-        <button className='btn comment-footer-button-text'>reply</button>
+        <button
+          className='btn comment-footer-button-text'
+          onClick={handleReplyButtonPress}
+        >
+          reply
+        </button>
       </div>
       <div>
         {comment.children.map((child: CommentType) => (
